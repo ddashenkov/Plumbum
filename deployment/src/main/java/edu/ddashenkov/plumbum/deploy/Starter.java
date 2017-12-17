650152d1b475de4d7da4f7c1defa72cfa7fed5b3
Dmytro Dashenkov <ymhik98@gmail.com>
2017-12-17 13:23:02 +0200

    Restore options request.

--- deployment/src/main/java/edu/ddashenkov/plumbum/deploy/Starter.java
+++ deployment/src/main/java/edu/ddashenkov/plumbum/deploy/Starter.java
@@ -9,6 +9,7 @@ import java.util.stream.Stream;
 import static java.lang.Integer.parseInt;
 import static java.lang.System.getenv;
 import static spark.Spark.before;
+import static spark.Spark.options;
 import static spark.Spark.port;
 
 final class Starter {
@@ -29,11 +30,27 @@ final class Starter {
     }
 
     private static void enableCORS() {
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+
         before((request, response) -> {
             final String frontend = "https://obscure-lowlands-96477.herokuapp.com";
             response.header("Access-Control-Allow-Origin", frontend);
-            response.header("Access-Control-Request-Method", "*");
-            response.header("Access-Control-Allow-Headers", "*");
+            response.header("Access-Control-Request-Method", frontend);
+            response.header("Access-Control-Allow-Headers", frontend);
             response.header("Access-Control-Allow-Credentials", "true");
             response.type("application/json");
         });
