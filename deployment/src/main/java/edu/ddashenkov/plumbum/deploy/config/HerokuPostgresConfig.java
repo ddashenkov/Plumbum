975dad06a202f6a86b12ce78ed96bf8b15c624ea
Dmytro Dashenkov <ymhik98@gmail.com>
2017-12-19 22:43:37 +0200

    Pass DataSource args one-by-one.

--- deployment/src/main/java/edu/ddashenkov/plumbum/deploy/config/HerokuPostgresConfig.java
+++ deployment/src/main/java/edu/ddashenkov/plumbum/deploy/config/HerokuPostgresConfig.java
@@ -20,11 +20,13 @@ public final class HerokuPostgresConfig {
     }
 
     private static DataSource herokuDataSource() {
-        final String url = "postgres://wrueucyquwohzk:6a13ae2fd05cb8e4e263a4c21e3d05c3c78ee45fca3aaa69b2ca0808e1f43ca8" +
-                "@ec2-54-235-244-185.compute-1.amazonaws.com:5432/dd749jf1trjnqu";
-        //System.getenv("DATABASE_URL");
         final PGSimpleDataSource dataSource = new PGSimpleDataSource();
-        dataSource.setUrl(url);
+        dataSource.setDatabaseName("dd749jf1trjnqu");
+        dataSource.setUser("wrueucyquwohzk");
+        dataSource.setPassword("6a13ae2fd05cb8e4e263a4c21e3d05c3c78ee45fca3aaa69b2ca0808e1f43ca8");
+        dataSource.setUrl("ec2-54-235-244-185.compute-1.amazonaws.com");
+        dataSource.setPortNumber(5432);
+        dataSource.setSsl(true);
         return dataSource;
     }
 }
